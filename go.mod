d489f6984ce7224839da88f2d9f9cabe890a85a7
Nikolay Konkin <nickvladimirovitch@gmail.com>
2026-09-05 03:01:32 +0500

    feat(auth): implement bcrypt password hashing
    - HashPassword: bcrypt.GenerateFromPassword with cost=10
    - VerifyPassword: bcrypt.CompareHashAndPassword, argument order is
      (hashedPassword, password) - hash first

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module advanced-blog-management-system
 
-go 1.25.0
+go 1.26.0
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
@@ -13,7 +13,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.30.4 // indirect
 	github.com/leodido/go-urn v1.5.0 // indirect
-	golang.org/x/crypto v0.55.0 // indirect
+	golang.org/x/crypto v0.56.0 // indirect
 	golang.org/x/sys v0.47.0 // indirect
 	golang.org/x/text v0.41.0 // indirect
 )
--- pkg/auth/password.go
+++ pkg/auth/password.go
@@ -1,18 +1,18 @@
 package auth
 
+import "golang.org/x/crypto/bcrypt"
+
 // HashPassword создает хеш пароля используя bcrypt
 func HashPassword(password string) (string, error) {
-	// TODO: Реализовать хеширование пароля
-	// 1. Использовать bcrypt.GenerateFromPassword с cost = 10
-	// 2. Вернуть хеш пароля в виде строки
-	// 3. Если ошибка - вернуть ошибку
-	return "", nil
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", err
+	}
+	return string(hashed), nil
 }
 
-// VerifyPassword проверяет что пароль соответствует хешу
+// VerifyPassword проверяет, что пароль соответствует хешу
 func VerifyPassword(hashedPassword, password string) bool {
-	// TODO: Реализовать проверку пароля
-	// 1. Использовать bcrypt.CompareHashAndPassword
-	// 2. Вернуть true если пароль совпадает, false иначе
-	return false
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
 }
